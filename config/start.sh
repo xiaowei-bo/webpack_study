8f09b7a1f3673985ff7f22af5357b30622f3db71
魏毅博 <yibo.wei@17zuoye.com>
2020-10-22 20:41:35 +0800

    暂存

--- config/start.sh
+++ config/start.sh
@@ -1,12 +1,31 @@
+current_path="$PWD"
 git pull
+if [ $? -ne 0 ]; then
+    echo "git pull failed";
+    exit 1;
+fi
 
 cnpm i
+if [ $? -ne 0 ]; then
+    echo "npm install failed";
+    exit 1;
+fi
 
 npm run build
+if [ $? -ne 0 ]; then
+    echo "build failed";
+    exit 1;
+fi
 
-current_path="$PWD"
 if [ "$(pm2 id myLife)" = "[]" ]; then
     pm2 start ${current_path}/config/pm2config.json
 else
     pm2 reload ${current_path}/config/pm2config.json
 fi
+if [ $? -ne 0 ]; then
+    echo "serve start failed";
+    exit 1;
+fi
+
+pm2 list
+
